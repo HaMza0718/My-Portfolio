5f82468c23b0c16d6601d987f64f45195e1f89c6
Hamza Yasir <hamzayasir677@gmail.com>
2023-10-03 00:05:29 +0500

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [nav, setNav] = useState(false);
 
-export default Header
+  const links = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "projects",
+    },
+    {
+      id: 4,
+      link: "skills",
+    },
+    {
+      id: 5,
+      link: "contact",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div>
+        <h1 className="text-3xl ml-2 font-signature cursor-context-menu hover:scale-75">
+          Hamza
+        </h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li className="px-4 cursor-pointer capitalize font-medium text-gray-300 hover:scale-105 duration-200">
+            {link}
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-1 z-10 text-gray-300 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-4 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-300">
+          {links.map(({ id, link }) => (
+            <li className="px-4 cursor-pointer capitalize py-6 text-4xl">
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Header;
